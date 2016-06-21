54dc611143184aa127a4007118115f4b748e1965
Abergard <littlinker@gmail.com>
2016-06-21 23:10:19 +0200

    reorganization

---
+++ .generate.py
@@ -0,0 +1,18 @@
+from external.ninja_generator.ninja_generator import NinjaGenerator
+
+project_root = "."
+main = "src"
+libs = {}
+exe = "Wolfenstein3D.exe"
+
+files = {"src": {"Camera", "Graphics", "main", "objectclass"}}
+
+includes = {"src": None}
+
+lflags = ("Gdi32.lib "
+          "User32.lib "
+          "glu32.lib "
+          "opengl32.lib")
+
+if __name__ == "__main__":
+    NinjaGenerator(project_root, files, libs, main, lflags, includes, exe)
